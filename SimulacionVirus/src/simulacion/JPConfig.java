39fc3a654e29e49bac208003d0d16c04eb05c030
Leon30 <portal.fernando@hotmail.com>
2020-09-24 17:06:03 -0500

    refractoring-config panel creation

---
+++ SimulacionVirus/src/simulacion/JPConfig.java
@@ -0,0 +1,19 @@
+package simulacion;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class JPConfig extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+
+
+	public JPConfig() {
+		initComps();
+	}
+
+
+	private void initComps() {
+		this.add(new JLabel());
+	}
+}
