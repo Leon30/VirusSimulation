3a85fc731374ad24adf04f05adbc1e67322e0085
Leon30 <portal.fernando@hotmail.com>
2020-09-24 16:40:43 -0500

    refreactoring

--- SimulacionVirus/src/simulacion/MainSim.java
+++ SimulacionVirus/src/simulacion/MainSim.java
@@ -34,17 +34,6 @@ public class MainSim {
 				e.printStackTrace();
 			}
 		}
-		final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-		//executorService.scheduleAtFixedRate(fixOverpos, 0, 1, TimeUnit.SECONDS);
-		t = new Timer(0, new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				fixOverpos();
-				t.stop();
-			}
-		});
-		t.start();
-		fixOverpos();
 	}
 	
 	public void fixOverpos() {
@@ -80,8 +69,6 @@ public class MainSim {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				generate();
-//				bodies[1] =	new Body(new PhysicsVector(r.nextDouble()*700, r.nextDouble()*680), new PhysicsVector(r.nextDouble()-0.5,r.nextDouble()-0.5), new PhysicsVector(0,0),r.nextDouble()*300,Color.GREEN);
-//				bodies[2] = new Body(new PhysicsVector(r.nextDouble()*700, r.nextDouble()*680), new PhysicsVector(r.nextDouble()-0.5,r.nextDouble()-0.5), new PhysicsVector(0,0),r.nextDouble()*300,Color.BLUE);
 			}
 		});
 		timer = new Timer(0,new ActionListener() {
@@ -98,28 +85,24 @@ public class MainSim {
 						if(body.checkOutside(jfMain.getJpDraw().getBounds())) {//Cuando sale
 							double rand = r2.nextDouble(); 
 							if(rand<0.25) {//generate on top
-								System.out.println("arriba");
 								PhysicsVector pv = new PhysicsVector();
 								pv.x = r2.nextDouble()*jfMain.getJpDraw().getWidth();
 								pv.y = -body.getSize();
 								body.repos(pv);
 								body.editVelocity(new PhysicsVector(0,0.5));
 							}else if(rand<0.5) {//derecha
-								System.out.println("derecha");
 								PhysicsVector pv = new PhysicsVector();
 								pv.x = jfMain.getJpDraw().getWidth()+body.getSize();
 								pv.y = r2.nextDouble()*jfMain.getJpDraw().getHeight();
 								body.repos(pv);
 								body.editVelocity(new PhysicsVector(-0.5,0));
 							}else if(rand<0.75) {//izquierda
-								System.out.println("left");
 								PhysicsVector pv = new PhysicsVector();
 								pv.x = -body.getSize();
 								pv.y = r2.nextDouble()*jfMain.getJpDraw().getHeight();
 								body.repos(pv);
 								body.editVelocity(new PhysicsVector(0.5,0));
 							}else {//abajo
-								System.out.println("abajo");
 								PhysicsVector pv = new PhysicsVector();
 								pv.x = r2.nextDouble()*jfMain.getJpDraw().getWidth();
 								pv.y = jfMain.getJpDraw().getWidth()+body.getSize();
